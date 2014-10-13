31561889b95087d3c11278aa0f77810722e3bf21
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-10-13 09:22:21 +0200

    update

--- src/main/java/fr/istic/model/Segment.java
+++ src/main/java/fr/istic/model/Segment.java
@@ -31,6 +31,4 @@ public class Segment {
 		this.val = val;
 	}
 	
-	
-
 }
--- src/main/java/fr/istic/view/ChartView.java
+++ src/main/java/fr/istic/view/ChartView.java
@@ -5,9 +5,16 @@ import java.awt.Graphics2D;
 
 import javax.swing.JComponent;
 
+import fr.istic.model.Chart;
+import fr.istic.model.IChart;
+
 public class ChartView extends JComponent {
 	
 	private static final long serialVersionUID = 1L;
+	
+	public ChartView(){
+		IChart chart = new Chart("Chart1");
+	}
 
 	protected void paintComponent(Graphics g){
 		
