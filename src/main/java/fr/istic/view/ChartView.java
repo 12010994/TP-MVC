79cf0dc8170442bf733bb2527afe060979f20d07
amandine <didinem2@gmail.com>
2014-10-13 10:27:02 +0200

    update chartview

--- src/main/java/fr/istic/view/ChartView.java
+++ src/main/java/fr/istic/view/ChartView.java
@@ -1,14 +1,42 @@
 package fr.istic.view;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Arc2D;
 
 import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+import com.sun.glass.events.WindowEvent;
 
 import fr.istic.model.IChart;
 
-public class ChartView extends JComponent implements IChartView{
+import java.awt.event.WindowAdapter;
+import java.awt.geom.*;
+
+public class ChartView extends JComponent{
+	
+
+
+	 public static void main(String[] args){
+	     ChartView chart= new ChartView();
+	     
+	   
+			JFrame f = new JFrame("Chart");
+			f.getContentPane().add( chart, "Center" );
+
+			f.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+					System.exit(0);
+				}
+			});
+			f.setSize(new Dimension(500, 450));
+			f.setVisible(true);
+		
+	    
+	   }
+	 
 	
 	private static final long serialVersionUID = 1L;
 	private IChart chart;
@@ -18,15 +46,21 @@ public class ChartView extends JComponent implements IChartView{
 	}
 
 	protected void paintComponent(Graphics g){
-		
-		Arc2D.Float arcChart = new Arc2D.Float(Arc2D.PIE);
-		arcChart.setFrame(70, 200, 150, 150);
-		arcChart.setAngleStart(0);
-		arcChart.setAngleExtent(145);
-		
 		Graphics2D graph = (Graphics2D) g;
+		
+		Arc2D.Double arcChart = new Arc2D.Double(Arc2D.PIE);
+		arcChart.setFrame(100, 100, 150, 150);
+		arcChart.setAngleStart(0);
+		arcChart.setAngleExtent(360);
+		
+		Arc2D.Double arcCenter = new Arc2D.Double(Arc2D.PIE);
+		arcCenter.setFrame(100, 100, 150, 150);
+		arcCenter.setAngleExtent(360);
+		
+
+		graph.draw(arcCenter);		
 		graph.draw(arcChart);
-		graph.drawLine(50, 50, 300, 300);
+		
 	}
 
 
