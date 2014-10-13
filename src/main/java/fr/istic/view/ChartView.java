fd5b165c7a66eee6ed91b928fa6d808cf0ea9783
amandine <didinem2@gmail.com>
2014-10-13 09:22:38 +0200

    update chartview

--- src/main/java/fr/istic/view/ChartView.java
+++ src/main/java/fr/istic/view/ChartView.java
@@ -8,7 +8,7 @@ import javax.swing.JComponent;
 import fr.istic.model.Chart;
 import fr.istic.model.IChart;
 
-public class ChartView extends JComponent {
+public class ChartView extends JComponent{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -18,8 +18,16 @@ public class ChartView extends JComponent {
 
 	protected void paintComponent(Graphics g){
 		
+		Arc2D.Float arcChart = new Arc2D.Float(Arc2D.PIE);
+		arcChart.setFrame(70, 200, 150, 150);
+		arcChart.setAngleStart(0);
+		arcChart.setAngleExtent(145);
+		
 		Graphics2D graph = (Graphics2D) g;
+		graph.draw(arcChart);
 		graph.drawLine(50, 50, 300, 300);
 	}
+
+
 
 }
