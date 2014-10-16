199c2d619aba3fa085a4b914a32683cfd93ad823
amandine <didinem2@gmail.com>
2014-10-16 15:24:23 +0200

    update

--- src/main/java/fr/istic/controller/Controller.java
+++ src/main/java/fr/istic/controller/Controller.java
@@ -1,8 +1,13 @@
 package fr.istic.controller;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Iterator;
 import java.util.Observable;
 import java.util.Observer;
+
+import javax.swing.JFrame;
 
 import fr.istic.model.Chart;
 import fr.istic.model.Segment;
@@ -24,11 +29,39 @@ public class Controller implements Observer{
 		selected = chart.getSegments().iterator();
 	}
 	
-	public static void main(final String[] args){
+	public static void main(final String[] args) {
+		
+		JFrame frame = new JFrame("test");
+		frame.setSize(500,500);
 		
 		Controller controller = new Controller();
 		
-		controller.chart.createSegment(100, "segment1", "Premier segment", "Blue");
+		frame.getContentPane().add(controller.chartView);
+
+		frame.setVisible(true);
+		
+		
+		controller.chart.createSegment(100, "segment1", "Test");
+		controller.chart.createSegment(500, "segment2", "testoi");
+		controller.chart.createSegment(300, "segment3", "Hello");
+		controller.chart.createSegment(300, "segment4", "Hello1");
+		controller.chart.createSegment(100, "segment53", "Hello2");
+		
+		
+
+		MouseListener l = new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				//e.getPos().x; e.getPos().y pour mouse listener 
+			    e.getX(); e.getY();
+			    System.out.println(e.getX());
+			    System.out.println(e.getY());
+			    //controller.chartView.
+				
+			}
+		};
+		frame.addMouseListener(l);
+		
+		
 	}
 
 	public void update(Observable o, Object arg) {
@@ -40,3 +73,4 @@ public class Controller implements Observer{
 	}
 	
 }
+
--- src/main/java/fr/istic/view/ChartView.java
+++ src/main/java/fr/istic/view/ChartView.java
@@ -1,24 +1,20 @@
 package fr.istic.view;
 
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Arc2D;
 
 import javax.swing.JComponent;
-import javax.swing.JFrame;
-
-import com.sun.glass.events.WindowEvent;
-import com.sun.prism.paint.Color;
 
 import fr.istic.model.Chart;
+import fr.istic.model.Segment;
 
-import java.awt.event.WindowAdapter;
 import java.awt.geom.*;
 import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
+
 
 public class ChartView extends JComponent implements Observer{
 
@@ -27,62 +23,73 @@ public class ChartView extends JComponent implements Observer{
 	private Chart chart;
 	private Rectangle2D rect;
 	private Line2D line;
-	 
+	
+	Color[] colors = {Color.BLUE, Color.CYAN, Color.GRAY, Color.GREEN, Color.DARK_GRAY, Color.LIGHT_GRAY};
+	int indcolor = 0;
+
 	public ChartView(Chart chart){
 		this.chart = chart;
 		chart.addObserver(this);
 	}
-
-	public static void main(String[] args){
-	     ChartView chart= new ChartView(new Chart("test"));
-	     
-			JFrame f = new JFrame("Chart");
-			f.getContentPane().add( chart, "Center" );
-
-			f.addWindowListener(new WindowAdapter() {
-				public void windowClosing(WindowEvent e) {
-					System.exit(0);
-				}
-			});
-			f.setSize(new Dimension(500, 450));
-			f.setVisible(true);
-		
-	   }
-
+	
+	public int nextColor(){
+		if(colors.length-1 <= indcolor){
+			indcolor = 0;
+		}else{
+			indcolor += 1;
+		}
+		return indcolor;
+	}
+	
 	protected void paintComponent(Graphics g) {
-		
-		
 		Graphics2D graph = (Graphics2D) g;
-		
+
 		Font font = new Font("Arial",Font.BOLD, 16);
 		graph.setFont(font);
-		int degres = 0;
+		int ecart = 0;
 		
-		for (int i =0; i<chart.getTotal(); i++){
-			//degres += 
+		for (Segment seg : chart.getSegments()){
+			
+			float degres = ((float) seg.getVal() /(float) chart.getTotal())*360;
+			
+			Arc2D.Double arcChart = new Arc2D.Double(0, 0, 90, 90, ecart, degres,Arc2D.PIE);
+			arcChart.setFrame(100, 100, 300, 300);
+			graph.setPaint(colors[indcolor]);
+
+			graph.fill(arcChart);
+			arcChart.contains(ecart, ecart+degres);
+			//System.out.println(arcChart.contains(ecart, ecart+degres));
+			
+			indcolor = nextColor();
+			ecart += degres;
 		}
-		Arc2D.Double arcChart = new Arc2D.Double(0, 0, 90, 0, 0, 90,Arc2D.PIE);
-		arcChart.setFrame(100, 100, 300, 300);
 		
+		//Allows separation between the segments and the center circle
+		graph.setPaint(Color.WHITE);
+		Arc2D.Double arcWhite = new Arc2D.Double(0, 0, 360, 0, 0, 360, Arc2D.PIE);
+		arcWhite.setFrame(150, 150, 200, 200);
+		graph.fill(arcWhite);	
+		
+		//center circle with the title and the total numerical values ​​of the fields.
+		graph.setPaint(Color.BLUE);
+		Arc2D.Double arcCenter = new Arc2D.Double(0, 0, 360, 0, 0, 360, Arc2D.PIE);
+		arcCenter.setFrame(175, 175, 150, 150);
+		graph.fill(arcCenter);	
+		
+		graph.setPaint(Color.WHITE);
+		graph.drawString(chart.getName(), 200,230);
+		graph.drawString(Integer.toString(chart.getTotal()), 200, 260);
 
-		
-		//Arc2D.Double arcCenter = new Arc2D.Double(0, 0, 360, 0, 0, 360, Arc2D.PIE);
-		//arcCenter.setFrame(150, 150, 200, 200);
-		
-		
-		//graph.drawString(chart.getName(), 200,230);
-		
 
-		//graph.draw(arcCenter);		
-		graph.fill(arcChart);
-		
-		
+
 	}
 
 	public void update(Observable o, Object arg) {
 		System.out.println("view updated");
-		paintComponent(getGraphics());
-		
+		//paintComponent(getGraphics());
+		repaint();
 	}
+	
 
 }
+
